2bb97f3ec108212eb497bb8ad7f49f1b8e54d804
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-13 20:22:56 -0400

    Assemble Move screen

--- src/move/MoveScreen.tsx
+++ src/move/MoveScreen.tsx
@@ -1,1 +1,9 @@
-export default function MoveScreen() { return null; }
+import { MoveHome } from '@/src/move/MoveHome';
+import { MovePlayer } from '@/src/move/MovePlayer';
+import { useMoveController } from '@/src/move/useMoveController';
+
+export default function MoveScreen() {
+  const move = useMoveController();
+  if (move.activeSession && move.runtime) return <MovePlayer move={move} />;
+  return <MoveHome move={move} />;
+}
